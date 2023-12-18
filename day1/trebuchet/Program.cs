c65090ff6abb15212306ecfa680d3aa27568e09d
Mathew Ross <31719422+matdross@users.noreply.github.com>
2023-12-18 13:12:11 +1000

    part one solution

---
+++ day1/trebuchet/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace trebuchet;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var lines = File.ReadAllLines("./trebuchet/input.txt");
+
+        var total = 0;
+        string? first = null!;
+        int position = 0;
+        string? last;
+
+        for (var i = 0; i < lines.Length; i++) {
+            var charArray = lines[i].ToCharArray();
+
+            for (var y = 0; y < charArray.Length; y ++) {
+                if (short.TryParse(charArray[y].ToString(), out var number)) {
+                    first ??= number.ToString();
+                    position = y;
+                }
+            }
+
+            last = charArray[position].ToString();
+
+            // both variables have to be a number
+            if (short.TryParse(first + last, out var lineNumberTotal)) {
+                total += lineNumberTotal;
+            }
+
+            first = null;
+            position = 0;
+        }
+
+        Console.WriteLine(total);
+    }
+}
